2df2587b16b9a1199211ffe209f27b1f6421a0b2
emy333 <emedoim333@gmail.com>
2023-05-11 13:49:43 -0300

    ajustes no layout

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-let msg = 'Hello word';
-console.log(msg);
+const product = [
+    {
+        id: 0,
+        image: 'assets/img/01.jpg',
+        title: 'Z Flip Foldable Mobile',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: 'assets/img/02.jpg',
+        title: 'Z Flip Foldable Mobile',
+        price: 130,
+    },
+    {
+        id: 2,
+        image: 'assets/img/01.jpg',
+        title: 'Z Flip Foldable Mobile',
+        price: 60,
+    },
+    {
+        id: 3,
+        image: 'assets/img/01.jpg',
+        title: 'Z Flip Foldable Mobile',
+        price: 50,
+    },
+    {
+        id: 4,
+        image: 'assets/img/01.jpg',
+        title: 'Z Flip Foldable Mobile',
+        price: 10,
+    }
+];
+
+const categories = [...new Set(product.map((item) =>
+    {return item }))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item) => 
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+            <div class='botton'>
+            <p>${title}</p>
+            <h2>R$ ${price}.00</h2>`+ 
+            "<button onclick='addtocart("+(i++)+")'>Adicionar ao carrinho</button>"+
+            `</div>
+            </div>`
+
+    )
+}).join('')
 
