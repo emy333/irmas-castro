65d74bede27744c6bc66a136abd050b3ab3645f3
emy333 <emedoim333@gmail.com>
2023-05-15 09:11:23 -0300

    add produtos no carrinho

--- index.js
+++ index.js
@@ -52,3 +52,31 @@ document.getElementById('root').innerHTML = categories.map((item) =>
     )
 }).join('')
 
+var cart = [];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+
+function displaycart(a){
+    let j = 0;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=> 
+        {
+            var {image, title, price} = items;
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size:15px;'>$ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+}
